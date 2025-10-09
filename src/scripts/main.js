a2f2de1abbe8d58c72857fadf9e74510be968fb1
João Henrique <70080335+JoaoHenriqueBR@users.noreply.github.com>
2025-10-09 20:25:01 -0300

    Aula 02 - A memória do herói
    Criado menu inicial e inicio do tabuleiro.

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+// Dom elements
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start');
+
+// Configurando um menu
+for (let i = 4; i <= 10; i += 2){ // De 4 até 10
+    let n = i*i; // Recebe i²
+    // Criar uma nova opção para o Select
+    let op = document.createElement('option');
+
+    // Definir tanto o valor quanto o seu conteúdo para o i²
+    op.value = n;
+    op.innerHTML = n;
+
+    // Adiciona a nova opção para a página
+    select.appendChild(op);
+}
